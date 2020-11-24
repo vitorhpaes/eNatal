74f89acd7bfa2c7df453ce7707552f4495c66f09
Vitor Paes <vitor@aspsoftwares.com.br>
2020-11-24 19:34:34 -0300

    DONE - configurando db do servidor

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,4 +14,19 @@ module.exports = {
             directory: `${__dirname}/database/seeds`,
         }
     },
+    production: {
+        client: "mysql2",
+        connection: {
+            database: "christmas",
+            user: "admin_christmas",
+            password: "admin151617"
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: `${__dirname}/database/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/database/seeds`,
+        }
+    },
 }   
