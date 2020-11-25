0e4cac37327afd3b521824dbae352f18d110e199
Vitor Paes <vitor@aspsoftwares.com.br>
2020-11-24 21:30:51 -0300

    FIX - Corrigindo HTTPS ERROR - Cors ainda não solucionado

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const server_url = process.env.NODE_ENV === 'development' ? "http://localhost:3333/api" : "http://natalsolidariolages.online/api";
+const server_url = process.env.NODE_ENV === 'development' ? "http://localhost:3333/api" : "https://natalsolidariolages.online/api";
 const api = axios.create({
     baseURL: server_url
 })
